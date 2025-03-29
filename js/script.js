26ae73230ab159c71a6a6a2e109334af1aefb1a4
Abrikos <abrikos@Timas--MacBook-Pro.local>
2025-03-29 18:54:56 +0200

    udt

--- js/script.js
+++ js/script.js
@@ -20,7 +20,20 @@ function addField() {
     let container = document.getElementById("form-container");
     let newGroup = document.createElement("div");
     newGroup.className = "input-group";
-    newGroup.innerHTML = '<input type="text" placeholder="" class="name input-name-human" value="Башинський Сергій"> <button onclick="removeField(this)" class="button-all">Видалити</button>';
+    newGroup.innerHTML = `
+    <div class="label-input">
+    <label for="name">Імʼя, Прізвище</label>
+    <div class="label-input-human">
+    <input type="text" placeholder="" class="name input-name-human" id="name" value="Башинський Сергій">
+    </div>
+    </div>
+    <div class="label-input"> <label for="gender">Стать</label>
+    <select name="gneder" id="gender" class="gender-select">
+    <option value="male">Чол</option>
+    <option value="female">Жін</option>
+    </select>
+    </div>
+    <button onclick="removeField(this)" class="button-all">Видалити</button>`;
     container.appendChild(newGroup);
 }
 

f4aa83e62b3a31fafc5250d68a3f5ad1445a750d
Abrikos <abrikos@Timas--MacBook-Pro.local>
2025-03-29 13:39:43 +0200

    Add

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function toggleEdit(editable) {
+    document.querySelectorAll("#form-container input").forEach(input => {
+        input.disabled = !editable;
+        if (!editable) {
+            input.classList.add("saved");
+        } else {
+            input.classList.remove("saved");
+        }
+    });
+    document.querySelectorAll(".input-group").forEach(group => {
+        if (!editable) {
+            group.style.display = "block";
+        } else {
+            group.style.display = "flex";
+        }
+    });
+}
+
+function addField() {
+    let container = document.getElementById("form-container");
+    let newGroup = document.createElement("div");
+    newGroup.className = "input-group";
+    newGroup.innerHTML = '<input type="text" placeholder="Имя" class="name"> <input type="text" placeholder="Фамилия" class="surname"> <button onclick="removeField(this)" class="button-all">Удалить</button>';
+    container.appendChild(newGroup);
+}
+
+function removeField(button) {
+    let container = document.getElementById("form-container");
+    if (container.children.length > 1) {
+        button.parentElement.remove();
+    }
+}
